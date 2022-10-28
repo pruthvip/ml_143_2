4e6f16063365bca7517d3568a7a06eec37e93db3
atp <pruthvip15@gmail.com>
2022-10-28 18:52:24 +0530

    Add mobile support for the site
    Add mobile support for the site

--- app.js
+++ app.js
@@ -7,20 +7,30 @@ const btnHeight = noBtn.getBoundingClientRect().height;
 const width = screen.width - btnWidth;
 const height = screen.height - btnHeight;
 
-noBtn.addEventListener('mouseover', (e) => {
-    let x = (e.clientX + btnWidth + Math.random() * 10) % width
-    let y = (e.clientY + btnHeight + Math.random() * 10) % height
+const eventsToListen = ['mouseover', 'click'];
 
-    noBtn.style.left = `${Math.floor((x))}px`;
-    noBtn.style.top = `${Math.floor((y))}px`;
+eventsToListen.forEach(event => {
+    noBtn.addEventListener(event, (e) => {
+        let x = (e.clientX + btnWidth + Math.random() * 10) % width
+        let y = (e.clientY + btnHeight + Math.random() * 10) % height
+    
+        noBtn.style.left = `${Math.floor((x))}px`;
+        noBtn.style.top = `${Math.floor((y))}px`;
+    });
+});
+
+const yesBtn = document.getElementById('yes-button');
+const responseSection = document.getElementById('js-response');
+const negativeBtnClick = document.getElementById('js-negative-action-container');
+
+
+yesBtn.addEventListener('click', () => {
+    noBtn.style.display = 'none';
+    negativeBtnClick.style.display = 'none';
+    responseSection.classList.add('response--show');
 });
 
 
-const yesBtn = document.getElementById('yes-button');
-const responseSection = document.getElementById('js-response')
 
-yesBtn.addEventListener('click', () => {
-    noBtn.style.left = 'initial';
-    noBtn.style.top = 'initial';
-    responseSection.classList.add('response--show')
-})
+
+
